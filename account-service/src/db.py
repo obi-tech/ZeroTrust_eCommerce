1da95318dc10e0ce85d53a67226cb9d54441b365
obi-tech <55135531+obi-tech@users.noreply.github.com>
2025-02-27 15:07:24 +0200

    Created a flask api file and dockerised account

--- account-service/src/db.py
+++ account-service/src/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB connection
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://localhost:27018")
 db = client["account_management"]
 
 def get_account_collection():
