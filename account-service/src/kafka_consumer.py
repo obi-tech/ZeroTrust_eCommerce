6b072fa2f05404e5f13162f05f36ef54006aed73
Xiru1024 <xiruchen1024@gmail.com>
2025-03-10 20:00:08 +0200

    Add account service to k8s

--- account-service/src/kafka_consumer.py
+++ account-service/src/kafka_consumer.py
@@ -1,11 +1,14 @@
+import os
+
 from kafka import KafkaConsumer
 import json
-
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
 consumer = KafkaConsumer(
     'account-events',
-    bootstrap_servers='localhost:9093',
+    bootstrap_servers=KAFKA_BROKER,
     auto_offset_reset='earliest',
     enable_auto_commit=True,
+    api_version=(3, 5, 0),
     group_id='account-service-group',
     value_deserializer=lambda v: json.loads(v.decode('utf-8'))
 )
--- account-service/src/kafka_producer.py
+++ account-service/src/kafka_producer.py
@@ -1,9 +1,15 @@
+import os
+
 from kafka import KafkaProducer
 import json
 
+KAFKA_BROKER = os.getenv("KAFKA_BROKER")  # Use Docker service name
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9093',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    bootstrap_servers=KAFKA_BROKER,
+    api_version=(3, 5, 0),
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    request_timeout_ms = 120000,
+    metadata_max_age_ms = 120000
 )
 
 def publish_event(topic, event):
